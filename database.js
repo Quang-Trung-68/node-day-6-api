4eac9d584619f95b350809ee91f85c90d1e5502d
Quang-Trung-68 <trungdang.dqt@gmail.com>
2026-02-03 15:12:20 +0700

    Refactor database connection setup to use dbConfig directly and add SSL configuration

--- database.js
+++ database.js
@@ -1,12 +1,6 @@
 const mysql = require("mysql2/promise");
 const dbConfig = require("@/configs/db.config");
 
-const db = mysql.createPool({
-  host: dbConfig.host, // Địa chỉ MySQL server
-  user: dbConfig.user, // Username để đăng nhập MySQL
-  password: dbConfig.password, // Password của user
-  database: dbConfig.database, // Tên database cần kết nối
-  port: dbConfig.port, // Port MySQL (mặc định 3306)
-});
+const db = mysql.createPool(dbConfig);
 
 module.exports = db;
